f01cbc692679c48e6ceba9c3e9a98e6a28ad629b
jaehyuneyea <jaehyune.yea@gmail.com>
2022-04-25 00:48:51 -0700

    Refactored the calculator functionality into an object

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
+
 let displayValue = 0;
 let operator;
 let val1 = 0;
 
-function add(a,b) {
-  return a + b;
-}
-
-function subtract(a,b) {
-  return a - b;
-}
-
-function multiply(a,b) {
-  return a * b;
-}
-
-function divide(a,b) {
-  return a / b;
-}
-
-function operate(op, a, b) {
-  switch (op) {
-    case '+':
-      return add(a,b);
-    case '-':
-      return subtract(a,b);
-    case '*':
-      return multiply(a,b);
-    case '/':
-      return divide(a,b);
-  }
-}
+let calc = new Calculator(displayValue, val1, operator);
 
 const display = document.querySelector('.display');
-display.textContent = displayValue;
+display.textContent = calc.displayValue;
 
 let buttons = document.querySelectorAll('.buttons');
 buttons.forEach(button => {
@@ -97,36 +71,35 @@ function update(key) {
 
     // CLEAR CASE
   if (key == 'clear') {
-    displayValue = 0; // might need to come back to it to change stored values
-    val1 = 0;
-    operator = undefined;
+    calc.displayValue = 0; // might need to come back to it to change stored values
+    calc.val1 = 0;
+    calc.operator = undefined;
   
     // OPERATORS CASE
   } else if (key == '+' || key == '-' || key == '*' || key == '/') {
-    if (val1 != 0) {
-      displayValue = operate(key, Number(val1), Number(displayValue)); // if val1 is not empty (meaning a previous calculation was done), calculate that first and display it before overriding.
+    if (calc.val1 != 0) {
+      calc.displayValue = calc.operate(key, Number(calc.val1), Number(calc.displayValue)); // if val1 is not empty (meaning a previous calculation was done), calculate that first and display it before overriding.
     }
-    operator = key;
-    val1 = displayValue;
+    calc.operator = key;
+    calc.val1 = calc.displayValue;
 
     // EQUALS CASE
   } else if (key == '=') {
-    if (val1 == 0 || displayValue == 0) return; // prevents displayvalue from disappearing when it is = 0
-    displayValue = operate(operator, Number(val1), Number(displayValue)); // compute the value
-    val1 = 0; // set val1 to zero because once equal is clicked we know the compute is finished until further numbers or operators are entered
+    if (calc.val1 == 0 || calc.displayValue == 0) return; // prevents displayvalue from disappearing when it is = 0
+    calc.displayValue = calc.operate(calc.operator, Number(calc.val1), Number(calc.displayValue)); // compute the value
+    calc.val1 = 0; // set val1 to zero because once equal is clicked we know the compute is finished until further numbers or operators are entered
 
     // FIRST NUMBER CASE
-  } else if ((displayValue == 0 && !isNaN(Number(key))) || (operator != undefined && val1 == displayValue )) { // if displayvalue is 0 and key is not = OR some operator is ready and val1 is same as displayValue (means value is on standby since displayValue copies its value to val1)
+  } else if ((calc.displayValue == 0 && !isNaN(Number(key))) || (calc.operator != undefined && calc.val1 == calc.displayValue )) { // if displayvalue is 0 and key is not = OR some operator is ready and val1 is same as displayValue (means value is on standby since displayValue copies its value to val1)
 
-    displayValue = Number(key);
+    calc.displayValue = Number(key);
 
     // NON FIRST NUMBER CASE
   } else {
-    displayValue = String(displayValue) + key; // turn display number into string and append the following numbers onto it
+    calc.displayValue = String(calc.displayValue) + key; // turn display number into string and append the following numbers onto it
 
   }
-  display.textContent = displayValue; // display after any possible operations are finished
+  display.textContent = calc.displayValue; // display after any possible operations are finished
 
 }
 
-module.exports = calculator;
